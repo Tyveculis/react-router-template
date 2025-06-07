3aca77562e1506acf342775f12f881230ca29dad
Tyveculis <66331807+Tyveculis@users.noreply.github.com>
2025-06-07 17:53:02 -0400

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
-import { GardenHome} from "../garden/home";
+import { App } from "../garden/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,5 +11,5 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
 //  return <Welcome />;
-    return <GardenHome />;
+    return <App />;
 }
